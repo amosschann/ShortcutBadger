9386394975c08ea51bb18e7018cfc442bfa0bc13
白宇东 <summer88123@163.com>
2023-01-31 18:46:37 +0800

    适配荣耀

--- ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/HonorHomeBadger.java
+++ ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/HonorHomeBadger.java
@@ -14,7 +14,7 @@ import me.leolin.shortcutbadger.ShortcutBadgeException;
 /**
  * @author Jason Ling
  */
-public class HuaweiHomeBadger implements Badger {
+public class HonorHomeBadger implements Badger {
 
     @Override
     public void executeBadge(Context context, ComponentName componentName, int badgeCount) throws ShortcutBadgeException {
@@ -22,13 +22,13 @@ public class HuaweiHomeBadger implements Badger {
         localBundle.putString("package", context.getPackageName());
         localBundle.putString("class", componentName.getClassName());
         localBundle.putInt("badgenumber", badgeCount);
-        context.getContentResolver().call(Uri.parse("content://com.huawei.android.launcher.settings/badge/"), "change_badge", null, localBundle);
+        context.getContentResolver().call(Uri.parse("content://com.hihonor.android.launcher.settings/badge/"), "change_badge", null, localBundle);
     }
 
     @Override
     public List<String> getSupportLaunchers() {
         return Arrays.asList(
-                "com.huawei.android.launcher"
+                "com.hihonor.android.launcher"
         );
     }
 }
